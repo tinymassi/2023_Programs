3a64466321637e2cc7974e3ee649f7f795c07af0
Massimo <143848693+tinymassi@users.noreply.github.com>
2023-09-24 06:20:00 +0000

    adding text file to commit

--- cs19s23/daily_practice/hang_man/hang_man_main.cpp
+++ cs19s23/daily_practice/hang_man/hang_man_main.cpp
@@ -6,9 +6,10 @@
 int main() {
     std::ifstream words("word_list.txt");
     std::string input_char;
+    std::string key_word;
     bool game_is_valid = true;
-
-    
+    key_word = hang::pick_word(words);
+    std::cout << key_word << std::endl;
 
     while (game_is_valid) {
         std::cin >> input_char;

995df5582e91972cd8bbaeff522e2bd54b6c8a91
Massimo <143848693+tinymassi@users.noreply.github.com>
2023-10-04 06:03:46 +0000

    added fun hidden riddle to binary search tree

--- cs19s23/daily_practice/binary_search/tree_data_struct.cpp
+++ cs19s23/daily_practice/binary_search/tree_data_struct.cpp
@@ -11,12 +11,12 @@
     // BFS Breadth first search
 
 struct Node {
-    int data;
+    std::string data;
     Node* left_child;
     Node* right_child;
 };
 
-Node* create_node (int data) {
+Node* create_node (std::string data) {
     Node* new_node = new Node();
     new_node->data = data;
     new_node->left_child = nullptr;
@@ -29,7 +29,7 @@ void printTree_pre (Node* root) {  // Preorder algorithm kinda makes sense...
 
     if (root == nullptr) return;  // node we are on is null end this recursion
 
-    std::cout << root->data << std::endl;  // print data
+    std::cout << root->data;  // print data
     printTree_pre(root->left_child);  // print all left childs until nullptr
     printTree_pre(root->right_child);  // print all right childs until nullptr
 }
@@ -39,26 +39,29 @@ void printTree_in (Node* root) {  // inorder algorithm
     if (root == nullptr) return;  // node we are on is null end this recursion
 
     printTree_in(root->left_child);
-    std::cout << root->data << std::endl;
+    std::cout << root->data;
     printTree_in(root->right_child);
 }
 
 int main() {
     // Level 1
-    Node* root = create_node(1);
+    Node* root = create_node("works, ");
     // Level 2
-    root->left_child = create_node(2);
-    root->right_child = create_node(3);
+    root->left_child = create_node("in-");
+    root->right_child = create_node("sentence ");
     // Level 3
-    root->left_child->left_child = create_node(4);
-    root->left_child->right_child = create_node(5);
-    root->right_child->right_child = create_node(7);
-    root->right_child->left_child = create_node(6);
+    root->left_child->left_child = create_node("If ");
+    root->left_child->right_child = create_node("function ");
+    root->right_child->right_child = create_node("sense.");
+    root->right_child->left_child = create_node("this ");
     // Level 4
-    root->left_child->right_child->left_child = create_node(9);
-    root->right_child->right_child->left_child = create_node(15);
+    root->left_child->right_child->left_child = create_node("order ");
+    root->right_child->right_child->left_child = create_node("makes ");
 
-    printTree_pre(root);
+    // printTree_pre(root);
+    // std::cout << std::endl;
+    printTree_in(root);
+    std::cout << std::endl;
     
 
     return 0;

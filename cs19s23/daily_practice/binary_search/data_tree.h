5008fd0439511d136405e6339bbb913b122a4a60
Massimo <143848693+tinymassi@users.noreply.github.com>
2023-10-05 06:20:07 +0000

    super rough data tree header with added functions

--- cs19s23/daily_practice/binary_search/data_tree.h
+++ cs19s23/daily_practice/binary_search/data_tree.h
@@ -8,7 +8,24 @@ template <typename T> class tree {
     struct Node;
     public:
 
-    
+    tree () {  // default constructor
+        if (root == nullptr) {
+            root->data == input_data;
+            root->left_child = nullptr;
+            root->right_child = nullptr;
+            return root;
+        }
+    }
+
+    Node* create_node (T input_data) {  // make default constructor make the root from function call
+
+        Node* new_node = new Node();
+        new_node->left_child = nullptr;
+        new_node->right_child = nullptr;
+        new_node->data = input_data;
+
+        return new_node;
+    }
 
 
     private:
@@ -18,7 +35,7 @@ template <typename T> class tree {
         T data;
     }
     Node* root;
-}
+};
 
 
 }  // namespace tree

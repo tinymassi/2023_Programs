e41b5eb3c2350cf973b9ff73698a2c22eff28963
Massimo <143848693+tinymassi@users.noreply.github.com>
2023-10-23 05:54:12 +0000

    created header file for hash table project

---
+++ cs19s23/daily_practice/hash/hash.h
@@ -0,0 +1,28 @@
+#ifndef HASH_H
+#define HASH_H
+#include <iostream>
+#include <list>
+
+namespace hash {
+
+template <typename T> class hash_map {
+
+    private:
+    static const int table_size{};  // whats with static const?
+    std::list<std::pair<T, T>> table;
+
+    public:
+    bool is_table_empty() const;  // why const function what this mean?
+    void index_finder();
+    void insert(T key, T data);
+    void remove(T key);
+    T search_table(T key);
+    void print_table();
+
+
+
+};
+
+}  // namespace hash
+
+#endif  // HASH_H

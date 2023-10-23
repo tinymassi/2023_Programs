a426129ae6448298a476926537e08bfdb6a78df2
Massimo <143848693+tinymassi@users.noreply.github.com>
2023-10-23 05:55:40 +0000

    fixed some errors in hash.h

--- cs19s23/daily_practice/hash/hash.h
+++ cs19s23/daily_practice/hash/hash.h
@@ -8,18 +8,35 @@ namespace hash {
 template <typename T> class hash_map {
 
     private:
+    
     static const int table_size{};  // whats with static const?
     std::list<std::pair<T, T>> table;
 
     public:
-    bool is_table_empty() const;  // why const function what this mean?
-    void index_finder();
-    void insert(T key, T data);
-    void remove(T key);
-    T search_table(T key);
-    void print_table();
 
+    bool is_table_empty() const {
 
+    }  // why const function what this mean?
+
+    void index_finder() {
+
+    }
+
+    void insert(T key, T data) {
+
+    }
+
+    void remove(T key) {
+
+    }
+
+    T search_table(T key) {
+
+    }
+
+    void print_table() {
+
+    }
 
 };
 

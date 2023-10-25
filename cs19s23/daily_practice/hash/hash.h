bcd9794d27eb64b8b206d6510c1c150d7af6c3bc
Massimo <143848693+tinymassi@users.noreply.github.com>
2023-10-25 05:40:03 +0000

    made is empty function

--- cs19s23/daily_practice/hash/hash.h
+++ cs19s23/daily_practice/hash/hash.h
@@ -10,12 +10,19 @@ template <typename T> class hash_map {
     private:
     
     static const int table_size{};  // whats with static const?
-    std::list<std::pair<T, T>> table;
+    std::list<std::pair<T, T>> table[table_size];
 
     public:
 
     bool is_table_empty() const {
+        bool is_empty = false;
+        for (int i = 0; i < table.size(); i++) {
+            if (table[i].size() == 0) {
+                return false;
+            }
+        }
 
+        return true;
     }  // why const function what this mean?
 
     void index_finder() {

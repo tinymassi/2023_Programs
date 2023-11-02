06f60c5aac3db11414aa8240d6d005ce35231a55
Massimo <143848693+tinymassi@users.noreply.github.com>
2023-11-02 05:11:39 +0000

    figuring out data structure...

--- cs19s23/daily_practice/diary_thing/logger.h
+++ cs19s23/daily_practice/diary_thing/logger.h
@@ -1,12 +1,14 @@
-#define LOGGER.H
 #ifndef LOGGER.H
+#define LOGGER.H
+#include "vector"
 
 namespace log {
 
 template <typename T> class log {
-    
-}
+    int static const size = 10000;
+    std::vector<std::vector<std::pair<int, std::list<std::pair<std::string, std::string>>>>> [size];
+};
 
-}  // namespace log
+} // namespace log
 
 #endif // LOGGER.H

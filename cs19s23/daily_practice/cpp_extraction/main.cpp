0b2883f97e4f8bcf4a355e7669b4e4c79fa55a6f
Massimo <143848693+tinymassi@users.noreply.github.com>
2023-11-10 05:43:43 +0000

    working with saving stuff in files

--- cs19s23/daily_practice/cpp_extraction/main.cpp
+++ cs19s23/daily_practice/cpp_extraction/main.cpp
@@ -4,7 +4,7 @@
 #include <fstream>
 
 void saveDataToFile(const std::vector<std::string>& container,const std::string& file_name) {
-    std::ofstream save_to_file (file_name);
+    std::ofstream save_to_file (file_name, std::ios::app);
     if (save_to_file.is_open()) {
         for (int i = 0; i < container.size(); i++) {
             save_to_file << container[i] << '\n';
@@ -16,10 +16,10 @@ void saveDataToFile(const std::vector<std::string>& container,const std::string&
     }
 }
 
-std::vector<std::string> loadDataFromFile (std::vector<std::string> &container, std::string& file_name) {
+void loadDataFromFile (std::vector<std::string>& container, std::string& file_name) {
     std::ifstream take_from_file (file_name);
     if (take_from_file.is_open()) {
-        std::string line;
+        std::string line{};
         while (std::getline(take_from_file, line)) {
             container.push_back(line);
         }
@@ -27,22 +27,33 @@ std::vector<std::string> loadDataFromFile (std::vector<std::string> &container, 
     } else {
         std::cerr << "Unable to open file: " << file_name << std::endl;
     }
-
 }
 
 int main() {
 
     std::cout << "Enter data:" << '\n';
     std::string text_file_name = "text_file.txt";
-    std::string input;
+    std::string input{};
     std::vector<std::string> container;
-    while (std::cin >> input) {
-        container.push_back(input);
+    bool check = true;
+    while (check) {
+        while (std::cin >> input) {
+            if (input == "end" || input == "End") {
+                check = false;
+                break;
+            } else {
+                container.push_back(input);
+            }
+        }
     }
 
     saveDataToFile(container, text_file_name);
 
-    std::vector<std::string> loaded_data = loadDataFromFile(container, text_file_name);
+    loadDataFromFile(container, text_file_name);
+
+    for (auto i : container) {
+        std::cout << i << '\n';
+    }
 
     return{};
 }

307095967e2b4ef98474b5add89c72c4ef9f09f7
Massimo <143848693+tinymassi@users.noreply.github.com>
2023-11-24 20:48:13 +0000

    added encrypt & decrypt functions (maybe bugs)

--- cs19s23/daily_practice/diary_thing/logger.h
+++ cs19s23/daily_practice/diary_thing/logger.h
@@ -374,6 +374,62 @@ class log {
         }
     }
 
+    std::string encrypt (std::string string) {
+        unsigned char ciphertext[128];  // an array of size 128 elementsused to store result of encryption
+        const unsigned char key[] = "0123456789abcdef";  // const unsigned char bc it relates to binary data its const bc this data shouldnt be modified
+        const unsigned char iv[] = "abcdefghijklmnop";  // This helps to randomize the encryption process
+        const unsigned char* binary_string = reinterpret_cast<const unsigned char*>(string.c_str());
+        EVP_CIPHER_CTX *ctx;  // a structure used to maintain information for cryptography such as the decryption or encryption algorithm
+        std::string encrypted_string;
+
+        ctx = EVP_CIPHER_CTX_new();  // allocated memory for a new EVP_CIPHER structure & returns pointer to created struct
+        EVP_EncryptInit_ex(ctx, EVP_aes_128_cfb(), NULL, key, iv);  // initializes encryption operation
+        // EVP_aes_128_cfb() is a function that creates an EVP_CYPHER object representing the algorithm used for encryption for key length of 128 bits
+
+        int len;  // used to store the length of the outuput after encryption process
+        int ciphertext_length;  // used to store the length of the ciphertext (encrypted data) after encryption process
+
+        EVP_EncryptUpdate(ctx, ciphertext, &len, binary_string, string.size());  // updates the encryption context ('ctx') by taking the password, and producing
+                                                                             // the encrypted output in the ciphertext buffer. &len is used to track # of bytes in update.
+
+        ciphertext_length = len;  // updates the cipher text length after EVP update
+
+        EVP_EncryptFinal_ex(ctx, ciphertext + len, &len);  // this produces the final encrypted output.
+
+        ciphertext_length += len;  // again updating the cipher text length
+
+        EVP_CIPHER_CTX_free(ctx);  // this deallocates the memory that was created with the EVP_CIPHER_CTX pointer structure
+
+        encrypted_string.assign(reinterpret_cast<const char*>(ciphertext), ciphertext_length);
+
+        return encrypted_string;
+    }
+
+    std::string decrypt (std::string encrypted_string) {
+        unsigned char decryptedtext[128];  // an array of size 128 elementsused to store result of encryption
+        const unsigned char key[] = "0123456789abcdef";  // const unsigned char bc it relates to binary data its const bc this data shouldnt be modified
+        const unsigned char iv[] = "abcdefghijklmnop";  // This helps to randomize the encryption process
+        const unsigned char* binary_encrypted_string = reinterpret_cast<const unsigned char*>(encrypted_string.c_str());
+        std::string decrypted_string;
+        EVP_CIPHER_CTX* ctx;
+        ctx = EVP_CIPHER_CTX_new();
+        EVP_DecryptInit_ex(ctx, EVP_aes_128_cfb(), NULL, key, iv);
+
+        int len;
+        int decryptedtext_length;
+
+        EVP_DecryptUpdate(ctx, decryptedtext, &len, binary_encrypted_string, encrypted_string.size());
+        decryptedtext_length = len;
+
+        EVP_DecryptFinal_ex(ctx, decryptedtext + len, &len);
+
+        decryptedtext_length += len;
+
+        decrypted_string.assign(reinterpret_cast<const char*>(decryptedtext), decryptedtext_length);
+
+        return decrypted_string;
+    }
+
     int hash_function(int password) {
         return password % size;
     }
